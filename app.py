24d6e4206469396da885cc42a4a17b1456cd075e
kavinghty <21153@rosmini.school.nz>
2026-03-16 07:34:20 +0000

    added flask and put html and css in folders

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+import sqlite3
+
+app = Flask(__name__)
+
+def get_db_connection():
+    conn = sqlite3.connect("database.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+@app.route("/")
+def home():
+    conn = get_db_connection()
+
+    user = conn.execute("SELECT * FROM Users LIMIT 1").fetchone()
+    routines = conn.execute("SELECT * FROM Routines").fetchall()
+    exercises = conn.execute("SELECT * FROM Exercise").fetchall()
+
+    conn.close()
+
+    return render_template(
+        "index.html",
+        user=user,
+        routines=routines,
+        exercises=exercises
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
