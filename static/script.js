200d2cdc05ac4da7dbbaa5176e4b5066cf36a4b6
kavinghty <21153@rosmini.school.nz>
2026-03-22 00:28:11 +0000

    Added page switching functionality

---
+++ static/script.js
@@ -0,0 +1,11 @@
+function showPage(pageId) {
+    const pages = document.querySelectorAll(".page");
+    pages.forEach(p => p.style.display = "none");
+
+    document.getElementById(pageId).style.display = "block";
+}
+
+// default page
+window.onload = function() {
+    showPage("dashboard");
+}
